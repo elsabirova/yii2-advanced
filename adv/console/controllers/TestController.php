4cbcdef571f85c79a7e03b5888fefbc384440dfe
elsabirova <grow_happy@mail.ru>
2019-02-17 23:36:56 +0100

    Create project

---
+++ adv/console/controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+namespace console\controllers;
+
+use yii\console\Controller;
+use yii\console\ExitCode;
+
+/**
+ * Test controller
+ */
+class TestController extends Controller
+{
+    public function actionIndex() {
+        $this->stdout('Hello, world!'.PHP_EOL);
+        return ExitCode::OK;
+    }
+}
---
+++ adv/frontend/controllers/TestController.php
@@ -0,0 +1,14 @@
+<?php
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Test controller
+ */
+class TestController extends Controller
+{
+    public function actionIndex() {
+        return $this->renderContent('Hello, world!');
+    }
+}
